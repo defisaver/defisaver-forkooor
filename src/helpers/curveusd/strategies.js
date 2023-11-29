a8fb721d25422f970e21ea2d2d000b56f9e1e562
majkic99 <vrnebojsamajkic@gmail.com>
2023-11-29 16:15:26 +0100

    add sub to boost strat

--- src/helpers/curveusd/strategies.js
+++ src/helpers/curveusd/strategies.js
@@ -33,6 +33,36 @@ async function subCurveUsdRepayBundle(
     return { subId, strategySub };
 }
 
+/**
+ * Subscribes to CurveUsd Boost Bundle
+ * @param {Object} owner eoa or proxy
+ * @param {number} bundleId CurveUsdRepay Bundle Id
+ * @param {string} controllerAddr address of the curveusd controller
+ * @param {number} maxRatio ratio over which the strategy will trigger
+ * @param {number} targetRatio target ratio for repay
+ * @returns {Object} subId and strategySub
+ */
+async function subCurveUsdBoostBundle(
+    owner, bundleId, controllerAddr, maxRatio, targetRatio
+) {
+    const [senderAcc, proxy] = await getSender(owner);
+    const triggerData = abiCoder.encode(["address", "address", "uint256", "uint8"], [proxy.address, controllerAddr, hre.ethers.utils.parseUnits(maxRatio.toString(), 16).toString(), 0]);
+    const ratioStateEncoded = abiCoder.encode(["uint8"], [0]);
+    const targetRatioEncoded = abiCoder.encode(["uint256"], [hre.ethers.utils.parseUnits(targetRatio.toString(), 16).toString()]);
+    const controllerAddressEncoded = abiCoder.encode(["address"], [controllerAddr]);
+    const controllerContract = new hre.ethers.Contract(controllerAddr, curveusdControllerAbi, senderAcc);
+    const collateralTokenAddress = await controllerContract.collateral_token();
+    const collTokenAddressEncoded = abiCoder.encode(["address"], [collateralTokenAddress]);
+    const crvUsdAddressEncoded = abiCoder.encode(["address"], ["0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E"]);
+    const strategySub = [bundleId, true, [triggerData],
+        [controllerAddressEncoded, ratioStateEncoded, targetRatioEncoded, collTokenAddressEncoded, crvUsdAddressEncoded]
+    ];
+    const subId = await subToStrategy(proxy, strategySub);
+
+    return { subId, strategySub };
+}
+
 module.exports = {
-    subCurveUsdRepayBundle
+    subCurveUsdRepayBundle,
+    subCurveUsdBoostBundle
 };

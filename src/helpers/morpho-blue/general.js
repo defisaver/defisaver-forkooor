dca6dca8e6fabc6e9e4701fad4feb6715a6a0c36
majkic99 <vrnebojsamajkic@gmail.com>
2024-02-05 12:27:17 +0100

    add supplying to morpho market in create position

--- src/helpers/morpho-blue/general.js
+++ src/helpers/morpho-blue/general.js
@@ -3,6 +3,12 @@ const dfs = require("@defisaver/sdk");
 const { getAssetInfoByAddress } = require("@defisaver/tokens");
 const { getSender, setBalance, approve, executeAction } = require("../../utils");
 const { getUserData } = require("./view");
+const { morphoBlueAbi } = require("../../abi/morpho-blue/abis");
+
+const morphoBlue = {
+    address: "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb",
+    abi: morphoBlueAbi
+};
 
 /**
  * Create a MorphoBlue position for sender on his proxy (created if he doesn't have one)
@@ -14,6 +20,8 @@ const { getUserData } = require("./view");
  */
 async function createMorphoBluePosition(marketParams, owner, coll, debt) {
     const [senderAcc, proxy] = await getSender(owner);
+
+    const [wallet] = await hre.ethers.getSigners();
 
     await setBalance(marketParams.collateralToken, owner, coll);
     await approve(marketParams.collateralToken, proxy.address, owner);
@@ -35,6 +43,16 @@ async function createMorphoBluePosition(marketParams, owner, coll, debt) {
 
     const functionData = createPositionRecipe.encodeForDsProxyCall()[1];
 
+    // supply loan token so there's enough liquidity
+    const morphoBlueContract = new hre.ethers.Contract(morphoBlue.address, morphoBlue.abi, wallet);
+
+    await setBalance(
+        marketParams.loanToken,
+        wallet.address,
+        amountDebt.mul(2)
+    );
+    await approve(marketParams.loanToken, morphoBlue.address, wallet.address);
+    await morphoBlueContract.supply(marketParams, amountDebt.mul(2), "0", wallet.address, [], { gasLimit: 3000000 });
     await executeAction("RecipeExecutor", functionData, proxy);
     return await getUserData(marketParams, proxy.address);
 }

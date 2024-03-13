e53ad4ef4bb30f1c2c4863c19c6fff9b823d0649
Rajko Zagorac <rajkozagorac00@gmail.com>
2024-03-13 14:28:46 +0100

    feat: add curveUsd payback strategy subscription endpoint

--- src/helpers/curveusd/strategies.js
+++ src/helpers/curveusd/strategies.js
@@ -62,7 +62,51 @@ async function subCurveUsdBoostBundle(
     return { subId, strategySub };
 }
 
+/**
+ * Subscribes to CurveUsd Payback Strategy
+ * @param {Object} owner eoa
+ * @param {number} strategyId CurveUsdPayback Strategy Id
+ * @param {string} controllerAddr address of the curveusd controller
+ * @param {number} minHealthRatio below this ratio strategy will trigger
+ * @param {number} amountToPayback amount of crvusd to payback
+ * @param {boolean} useBalanceFrom whether to use whole balance from owner when paying back
+ * @returns {Object} subId and strategySub
+ */
+async function subCurveUsdPaybackStrategy(
+    owner, strategyId, controllerAddr, minHealthRatio, amountToPayback, useBalanceFrom
+) {
+    const [, proxy] = await getSender(owner);
+
+    const curveUsdAddress = "0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E";
+    const amountToPaybackFormatted = hre.ethers.utils.parseUnits(amountToPayback.toString(), 18);
+    const minHealthRatioFormatted = hre.ethers.utils.parseUnits(minHealthRatio.toString(), 16);
+
+    const balanceAndAllowanceTriggerData = abiCoder.encode(
+        ["address", "address", "address", "uint256", "bool"],
+        [owner, proxy.address, curveUsdAddress, amountToPaybackFormatted, useBalanceFrom]
+    );
+
+    const crvUsdHealthRatioTriggerData = abiCoder.encode(
+        ["address", "address", "uint256"],
+        [proxy.address, controllerAddr, minHealthRatioFormatted]
+    );
+
+    const triggerData = [balanceAndAllowanceTriggerData, crvUsdHealthRatioTriggerData];
+
+    const controllerAddressEncoded = abiCoder.encode(["address"], [controllerAddr]);
+    const minHealthRatioEncoded = abiCoder.encode(["uint256"], [minHealthRatio.toString()]);
+    const amountToPaybackEncoded = abiCoder.encode(["uint256"], [amountToPaybackFormatted.toString()]);
+    const curveUsdAddressEncoded = abiCoder.encode(["address"], [curveUsdAddress]);
+    const subDataEncoded = [controllerAddressEncoded, minHealthRatioEncoded, amountToPaybackEncoded, curveUsdAddressEncoded];
+
+    const strategySub = [strategyId, false, triggerData, subDataEncoded];
+    const subId = await subToStrategy(proxy, strategySub);
+
+    return { subId, strategySub };
+}
+
 module.exports = {
     subCurveUsdRepayBundle,
-    subCurveUsdBoostBundle
+    subCurveUsdBoostBundle,
+    subCurveUsdPaybackStrategy
 };

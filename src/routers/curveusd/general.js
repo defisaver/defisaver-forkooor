237a4c0b90942f0101588acb944c3f752f6a19a5
Milos Djurica <milosdjurica.work@gmail.com>
2026-01-23 12:04:33 +0100

    feat: curveusd general routes refactor

--- src/routers/curveusd/general.js
+++ src/routers/curveusd/general.js
@@ -1,7 +1,7 @@
 /* eslint-disable consistent-return */
 /* eslint-disable jsdoc/check-tag-names */
 const express = require("express");
-const { setupVnet, getWalletAddr, defaultsToSafe } = require("../../utils");
+const { setupVnet, getSmartWallet, defaultsToSafe } = require("../../utils");
 const { body, validationResult } = require("express-validator");
 const { createCurveUsdPosition } = require("../../helpers/curveusd/general");
 const { getUserData } = require("../../helpers/curveusd/view");
@@ -23,33 +23,46 @@ const router = express.Router();
  *         application/json:
  *           schema:
  *             type: object
+ *             required:
+ *               - vnetUrl
+ *               - controller
+ *               - eoa
+ *               - collAmount
+ *               - debtAmount
+ *               - numberOfBands
  *             properties:
  *              vnetUrl:
  *                type: string
  *                example: "https://virtual.mainnet.eu.rpc.tenderly.co/7aedef25-da67-4ef4-88f2-f41ce6fc5ea0"
+ *                description: "Unique identifier for the vnet"
  *              controller:
  *                type: string
  *                example: "0xA920De414eA4Ab66b97dA1bFE9e6EcA7d4219635"
- *              owner:
+ *                description: "CurveUSD controller address"
+ *              eoa:
  *                type: string
  *                example: "0x499CC74894FDA108c5D32061787e98d1019e64D0"
- *              coll:
+ *                description: "The EOA which will be sending transactions and own the newly created wallet if walletAddr is not provided"
+ *              collAmount:
  *                type: number
  *                example: 2
- *              debt:
+ *                description: "Amount of collateral to supply in token units (e.g., 2 for 2 ETH). Supports float numbers (e.g., 2.5)"
+ *              debtAmount:
  *                type: number
  *                example: 2000
+ *                description: "Amount of crvUSD debt to borrow in token units (e.g., 2000 for 2000 crvUSD). Supports float numbers (e.g., 2000.25)"
  *              numberOfBands:
  *                type: number
  *                example: 10
- *              walletAddr:
+ *                description: "Number of bands for creating a new CurveUSD position"
+ *              smartWallet:
  *                type: string
  *                example: "0x0000000000000000000000000000000000000000"
- *                description: "The address of the wallet that will be used for the position, if not provided a new wallet will be created"
+ *                description: "Optional proxy address. If not provided, a new wallet will be created"
  *              walletType:
  *                type: string
  *                example: "safe"
- *                description: "Whether to use the safe as smart wallet or dsproxy. WalletType field is not mandatory. Defaults to safe"
+ *                description: "Whether to use Safe as smart wallet or dsproxy if smartWallet is not provided. Optional, defaults to safe"
  *     responses:
  *       '200':
  *         description: OK
@@ -119,24 +132,34 @@ const router = express.Router();
  *                   type: string
  */
 router.post("/create",
-    body(["vnetUrl", "controller", "coll", "debt", "owner", "numberOfBands"]).notEmpty(),
+    body(["vnetUrl", "controller", "eoa", "collAmount", "debtAmount", "numberOfBands"]).notEmpty(),
+    body(["collAmount", "debtAmount"]).isFloat({ gt: 0 }),
     async (req, res) => {
         const validationErrors = validationResult(req);
 
         if (!validationErrors.isEmpty()) {
             return res.status(400).send({ error: validationErrors.array() });
         }
-        const { vnetUrl, controller, owner, coll, debt, numberOfBands } = req.body;
 
-        await setupVnet(vnetUrl, [owner]);
-        createCurveUsdPosition(controller, coll, debt, owner, numberOfBands, getWalletAddr(req), defaultsToSafe(req))
-            .then(pos => {
-                res.status(200).send(pos);
-            })
-            .catch(err => {
-                res.status(500).send({ error: `Failed to create position info with error : ${err.toString()}` });
-            });
+        try {
+            const { vnetUrl, controller, eoa, collAmount, debtAmount, numberOfBands, smartWallet } = req.body;
 
+            await setupVnet(vnetUrl, [eoa]);
+
+            const pos = await createCurveUsdPosition(
+                controller,
+                collAmount,
+                debtAmount,
+                eoa,
+                numberOfBands,
+                smartWallet || getSmartWallet(req),
+                defaultsToSafe(req)
+            );
+
+            res.status(200).send(pos);
+        } catch (err) {
+            res.status(500).send({ error: `Failed to create CurveUSD position with error : ${err.toString()}` });
+        }
     });
 
 /**
@@ -154,16 +177,23 @@ router.post("/create",
  *         application/json:
  *           schema:
  *             type: object
+ *             required:
+ *               - vnetUrl
+ *               - controller
+ *               - positionOwner
  *             properties:
  *              vnetUrl:
  *                type: string
  *                example: "https://virtual.mainnet.eu.rpc.tenderly.co/7aedef25-da67-4ef4-88f2-f41ce6fc5ea0"
+ *                description: "Unique identifier for the vnet"
  *              controller:
  *                type: string
  *                example: "0xA920De414eA4Ab66b97dA1bFE9e6EcA7d4219635"
- *              owner:
+ *                description: "CurveUSD controller address"
+ *              positionOwner:
  *                type: string
  *                example: "0x45a933848c814868307c184F135Cf146eDA28Cc5"
+ *                description: "User owning the position. Specify either eoa, if eoa position, or wallet address if position is owned by a wallet"
  *     responses:
  *       '200':
  *         description: OK
@@ -233,23 +263,25 @@ router.post("/create",
  *                   type: string
  */
 router.post("/get-position",
-    body(["vnetUrl", "controller", "owner"]).notEmpty(),
+    body(["vnetUrl", "controller", "positionOwner"]).notEmpty(),
     async (req, res) => {
         const validationErrors = validationResult(req);
 
         if (!validationErrors.isEmpty()) {
             return res.status(400).send({ error: validationErrors.array() });
         }
-        const { vnetUrl, controller, owner } = req.body;
 
-        setupVnet(vnetUrl);
+        try {
+            const { vnetUrl, controller, positionOwner } = req.body;
 
-        getUserData(controller, owner)
-            .then(pos => {
-                res.status(200).send(pos);
-            }).catch(err => {
-                res.status(500).send({ error: `Failed to fetch position info with error : ${err.toString()}` });
-            });
+            await setupVnet(vnetUrl);
+
+            const pos = await getUserData(controller, positionOwner);
+
+            res.status(200).send(pos);
+        } catch (err) {
+            res.status(500).send({ error: `Failed to fetch CurveUSD position info with error : ${err.toString()}` });
+        }
     });
 
 module.exports = router;
